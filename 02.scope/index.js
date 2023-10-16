83e9a2e3d97fc1f2fc2ee0706bc0c54e476b94df
stanleyos <stanislaw.sarka@gmail.com>
2023-10-16 16:16:32 +0200

    Added scope concept and updated README

---
+++ 02.scope/index.js
@@ -0,0 +1,13 @@
+// Scope
+
+// Zarządzanie zmiennymi to jest fundamentalną cechą języka programowania i wymaga złozonego systemu zasad.
+// System ten nazywamy zakresem (scope)
+// Jego rolę mozemy sprowadzić do określenia gdzie i w jaki sposób zmienne mogą być odnalezione
+// Zmienne mogą być wyszukiwane na potrzeby przypisania referencji (tzw. LHS - left hand side look-up)
+//lub zwrócenia wartości (tzw. RHS - right hand side look-up).
+
+// Global Scope: Zmienne zdefiniowane poza jakimkolwiek blokiem lub funkcją są dostępne globalnie w całym skrypcie.
+// Są to zmienne, które można uznać za globalne, ponieważ są dostępne wszędzie.
+
+// Local Scope: Zmienne zdefiniowane wewnątrz bloku, funkcji lub pętli są dostępne tylko wewnątrz tego bloku, funkcji lub pętli.
+// Są to zmienne, które można uznać za lokalne.
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# concepts
+# Concepts
 My small glossary filled with important JS concepts
+
+
+## Execution context
+
+abstrakcyjny koncept środowiska w którym interpretowany i wykonywany jest kod JS
+
+
+## Scope
+
+określa gdzie i w jaki sposób zmienne mogą być odnalezione
